00cf285d64aca0b945cf0af0d42b2d58f72fa8df
Yazaar <41300736+Yazaar@users.noreply.github.com>
2020-03-05 19:14:07 +0100

    aaaaaaaaaaaaaaaa

--- sites/pokeapi/js/script.js
+++ sites/pokeapi/js/script.js
@@ -1,33 +1,47 @@
-var aerodactylPerks = {
-    strong: ['bug', 'fight', 'grass', 'fire', 'flying', 'ice'],
-    weak: ['electric', 'ice', 'rock', 'fight', 'grass', 'ground', 'steel', 'water'],
-    immune: ['ground']
-};
+var aerodactylPerks = {};
 
 var mainPokemonStats = {};
 
 var score = 0;
 
 function strongWeakImmune(type) {
-    for (var i = 0; i < aerodactylPerks.immune.length; i++) {
-        if (type === aerodactylPerks.immune[i]) {
-            return 'immune';
+    var response = {
+        from: {
+            weak: false,
+            strong: false,
+            immune: false
+        }, to: {
+            weak: false,
+            strong: false,
+            immune: false
         }
     }
-
-    for (var i = 0; i < aerodactylPerks.strong.length; i++) {
-        if (type === aerodactylPerks.strong[i]) {
-            return 'strong';
+    var keys = ['double_damage_from', 'double_damage_to', 'half_damage_from', 'half_damage_to', 'no_damage_from', 'no_damage_to'];
+    var aerodactylTypes = ['flying', 'rock'];
+    for (var i = 0; i < keys.length; i++) {
+        var i_key = keys[i];
+        for (var j = 0; j < aerodactylTypes.length; j++) {
+            var currentType = aerodactylTypes[j];
+            for (var k = 0; k < aerodactylPerks[currentType][i_key].length; k++) {
+                if (aerodactylPerks[currentType][i_key][j].name === type) {
+                    if (i === 0) {
+                        response.from.strong = true;
+                    } else if (i === 1){
+                        response.to.strong = true;
+                    } else if (i === 2){
+                        response.from.weak = true;
+                    } else if (i === 3){
+                        response.to.weak = true;
+                    } else if (i === 4){
+                        response.from.immune = true;
+                    } else if (i === 5){
+                        response.to.immune = true;
+                    }
+                }
+            }
         }
     }
-
-    for (var i = 0; i < aerodactylPerks.weak.length; i++) {
-        if (type === aerodactylPerks.weak[i]) {
-            return 'weak';
-        }
-    }
-
-    return 'neutral';
+    return response;
 }
 
 function pokeApiResponse(response){
@@ -40,18 +54,42 @@ function pokeApiResponse(response){
     typesElement.innerHTML = '';
     for (var i = 0; i < response.types.length; i++) {
         var pokeType = document.createElement('p');
-        pokeType.innerText = response.types[i].type.name;
+        var span1 = document.createElement('span');
+        span1.innerText = '⬤ ';
+        var span2 = document.createElement('span');
+        span2.innerText = response.types[i].type.name;
+        var span3 = document.createElement('span');
+        span3.innerText = ' ⬤';
         var pokePerk = strongWeakImmune(response.types[i].type.name);
-        if (pokePerk === 'immune') {
-            pokeType.classList.add('colorGreen');
+        // right attacks left
+        if (pokePerk.from.immune === true) {
             score += 25;
-        } else if (pokePerk === 'strong') {
-            score += 5;
-            pokeType.classList.add('colorBlue');
-        } else if (pokePerk === 'weak') {
+            span3.classList.add('colorGreen');
+        } else if (pokePerk.from.strong === true && pokePerk.from.weak === true){
+            // strong and weak cancels each other out
+        }else if (pokePerk.from.strong === true) {
             score -= 5;
-            pokeType.classList.add('colorRed');
+            span3.classList.add('colorBlue');
+        } else if (pokePerk.from.weak === true) {
+            score += 5;
+            span3.classList.add('colorRed');
         }
+        // left attacks right
+        if (pokePerk.to.immune === true) {
+            score -= 25;
+            span1.classList.add('colorGreen');
+        } else if (pokePerk.to.strong === true && pokePerk.to.weak === true) {
+            // strong and weak cancels each other out
+        } else if (pokePerk.to.strong === true) {
+            score += 5;
+            span1.classList.add('colorBlue');
+        } else if (pokePerk.to.weak === true) {
+            score -= 5;
+            span1.classList.add('colorRed');
+        }
+        pokeType.appendChild(span1);
+        pokeType.appendChild(span2);
+        pokeType.appendChild(span3);
         typesElement.appendChild(pokeType);
     }
     
@@ -151,7 +189,6 @@ function pokeApiRequest(endpoint, callback) {
     xml.send();
 }
 
-
 // 142 = Aerodactyl
 pokeApiRequest('pokemon/142', function(response){
     var AerodactylImage = response.sprites.front_default;
@@ -163,6 +200,9 @@ pokeApiRequest('pokemon/142', function(response){
         var pokeType = document.createElement('p');
         pokeType.innerText = response.types[i].type.name;
         typesElement.appendChild(pokeType);
+        pokeApiRequest('type/' + response.types[i].type.name, function(response){
+            aerodactylPerks[response.name] = response.damage_relations;
+        });
     }
     document.querySelector('.left .baseXP').innerText = response.base_experience;
     mainPokemonStats.baseXP = response.base_experience;
